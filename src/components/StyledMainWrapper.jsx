ca211ba4b41aa87bf1183de422818d071f2d69e5
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2023-10-03 22:35:49 +0530

    fixed

---
+++ src/components/StyledMainWrapper.jsx
@@ -0,0 +1,19 @@
+'use client';
+import styled from 'styled-components';
+
+export default function StyledMainWrapper({ children }) {
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 800px;
+  padding: 16px 24px;
+  margin: 0px auto;
+  border: 1px solid hsl(0deg 0% 50% / 0.3);
+  border-radius: 2px;
+  background: white;
+`;
+
+    return (
+        <MainWrapper>{children}</MainWrapper>
+    )
+}
